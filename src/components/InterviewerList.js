935fe9013418528b45233bee13433ccb7fb44826
abdul5890575 <abdulrehman.shoukat@gmail.com>
2021-04-13 15:38:48 -0400

    components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,7 +4,7 @@ import InterviewerListItem from "components/InterviewerListItem"
 
 export default function InterviewerList(props) {
   let interviewerList = props.interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} name={interviewer.name} 
-  selected = {interviewer.id === props.interviewer} setInterviewer={props.setInterviewer} avatar = {interviewer.avatar}/>)  
+  selected = {interviewer.id === props.interviewer} setInterviewer={(event)=>{props.setInterviewer(interviewer.id)}} avatar = {interviewer.avatar}/>)  
 
   return (
     <section className="interviewers">
