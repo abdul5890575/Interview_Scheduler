eb4a5418794b2889cc5d6aafddcab8d976d08d74
abdul5890575 <abdulrehman.shoukat@gmail.com>
2021-04-14 14:25:59 -0400

    days api added

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,27 @@
-import React ,{ useState } from "react";
+import React ,{ useState, useEffect } from "react";
+import axios from "axios"
 
 import "components/Application.scss";
 import DayList from "components/DayList"
 import Appointment from "components/Appointment"
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = [
   {
@@ -67,7 +68,16 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [Day, setDay] = useState("Monday");
+  const [Day, setDay] = useState("");
+  const [days, setDays] = useState([]);
+  
+
+  useEffect(() => {
+   
+    axios.get('/api/days').then(response => {
+      setDays([...response.data])
+    });
+  }, [])
 
  
   let ListofAppointments = appointments.map(appointment => <Appointment key={appointment.id}  {...appointment} />) 
