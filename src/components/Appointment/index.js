ecd12472e327b44d6c40021620692a6b7509de71
abdul5890575 <abdulrehman.shoukat@gmail.com>
2021-04-15 18:30:30 -0400

    history

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,13 +3,20 @@ import "components/Appointment/styles.scss"
 import Header from "components/Appointment/Header"
 import Show from "components/Appointment/Show"
 import Empty from "components/Appointment/Empty"
+import useVisualMode from "hooks/useVisualMode"
 
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+  const { mode, transition, back } = useVisualMode( props.interview ? SHOW : EMPTY);
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> :<Empty /> }
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && ( <Show student={props.interview.student} interviewer={props.interview.interviewer}/>
+)}
     </article>
   );
 }
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,34 @@
+import React ,{ useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+//   const [index,setIndex] = useState(0)
+
+  const transition = (newmode, replace = false) =>{
+    // setMode(newmode)
+    if (replace) {
+        setMode(newmode)
+        let shortHistory = history
+        shortHistory.pop()
+        setHistory([...shortHistory,newmode])
+    } else {
+        setMode(newmode)
+        const newHistory = [...history,newmode]
+        setHistory(newHistory)
+    }
+  }
+
+  
+  const back = () =>{
+    if (history.length === 1) {
+      return
+    } else{
+        let shortHistory = history
+        shortHistory.pop()
+        setMode(shortHistory[shortHistory.length-1] );
+        setHistory(shortHistory)
+    }
+  }
+    return { mode , transition, back};
+}
